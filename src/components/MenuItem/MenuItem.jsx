7f036230488fdd41d27fc67f44bf56c810b3dd13
Atieh Gholipour <Atiyeh.gholipour71@gmail.com>
2025-01-14 21:00:22 +0100

    the task is done.

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -2,16 +2,18 @@ import { useNavigate } from "react-router-dom";
 import Button from "../Button/Button";
 import styles from "./MenuItem.module.css";
 
-const MenuItem = ({ dish }) => {
+const MenuItem = ({ dish, isInWishlist, onToggleWishlist }) => {
   const { strMeal: name, strMealThumb: image } = dish;
   const navigate = useNavigate();
+
   return (
     <div className={styles.menuItem}>
       <h3>{name}</h3>
       <img src={image} alt={name} />
       <div className={styles.menuItemBtnContainer}>
-        <Button onClick={() => navigate(`/meals/${dish.idMeal}`)}>
-          Details
+        <Button onClick={() => navigate(`/meals/${dish.idMeal}`)}>Details</Button>
+        <Button onClick={() => onToggleWishlist(dish)}>
+          {isInWishlist ? "Remove from Wishlist" : "Add to Wishlist"}
         </Button>
       </div>
     </div>
--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import styles from "./SearchField.module.css";
 
-const SearchField = () => {
-  
+const SearchField = ({ onSearchChange }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+    onSearchChange(event.target.value);
+  };
+
   return (
     <div className={styles.wrapper}>
       <input
         placeholder="Filter dishes..."
-        type="text"     
+        type="text"
+        value={searchTerm}
+        onChange={handleInputChange}
       />
     </div>
   );
